b9f891d15ac405fda301087e61a785e6abee5269
OlivierGitHub <olivier_coulibaly@yahoo.fr>
2015-08-21 01:15:33 +0200

    Angular development
    in progress

--- web/girl/girl.js
+++ web/girl/girl.js
@@ -1,3 +1,10 @@
 /**
  * Created by oliver on 21/08/15.
  */
+
+/* global angular */
+angular.module('girlApp', [])
+
+    .controller('girlCtrl', ['$scope', function ($$scope) {
+        'use strict';
+    }]);
--- web/index.js
+++ web/index.js
@@ -1,3 +1,18 @@
 /**
  * Created by oliver on 21/08/15.
  */
+
+/*global angular */
+angular.module('app', ['ui.router', 'girlApp'])
+
+    .config(function($urlRouterProvider, $stateProvider){
+        'use strict';
+
+        $urlRouterProvider.otherwise("/home");
+
+        $stateProvider
+            .state('girl', {
+                url: '/choose_a_girl',
+                templateUrl: 'girl/girl.html'
+            });
+    });
